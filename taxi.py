17f1138be7171b67f624e4238b9a07fcf2d26aae
Humza99 <99362244+Humza99@users.noreply.github.com>
2022-11-30 14:54:01 +0000

    Add files via upload
    A* working but not recording in taxi account.

--- taxi.py
+++ taxi.py
@@ -332,39 +332,49 @@ class Taxi:
       # this function should build your route and fill the _path list for each new
       # journey. Below is a naive depth-first search implementation. You should be able
       # to do much better than this!
-      def _planPath(self, origin, destination, **args):
-          # the list of explored paths. Recursive invocations pass in explored as a parameter
-          if 'explored' not in args:
-             args['explored'] = {}
-          # add this origin to the explored list
-          # explored is a dict purely so we can hash its index for fast lookup, so its value doesn't matter
-          args['explored'][origin] = None 
-          # the actual path we are going to generate
-          path = [origin]
-          # take the next node in the frontier, and expand it depth-wise               
-          if origin in self._map:
-             # the frontier of unexplored paths (from this Node
-             frontier = [node for node in self._map[origin].keys() if node not in args['explored']]
-             # recurse down to the next node. This will automatically create a depth-first
-             # approach because the recursion won't bottom out until no more frontier nodes
-             # can be generated 
-             for nextNode in frontier:
-                 path = path + self._planPath(nextNode, destination, explored=args['explored'])
-                 # stop early as soon as the destination has been found by any route.
-                 if destination in path:
-                    # validate path
-                    if len(path) > 1:
-                       try:
-                           # use a generator expression to find any invalid nodes in the path
-                           badNode = next(pnode for pnode in path[1:] if pnode not in self._map[path[path.index(pnode)-1]].keys())
-                           raise IndexError("Invalid path: no route from ({0},{1}) to ({2},{3} in map".format(self._map[path.index(pnode)-1][0], self._map[path.index(pnode)-1][1], pnode[0], pnode[1]))
-                       except StopIteration:
-                           pass
-                    return path
-          # didn't reach the destination from any reachable node
-          # no need, therefore, to expand the path for the higher-level call, this is a dead end.
-          return [] 
-                
+
+      # A* search is an informed search, and expects a heuristic, which should be a
+      # function of 2 variables, both tuples, the start, and the target.
+      def _planPath(self, origin, destination, heuristic=None):
+          if origin not in self._map:
+             return None
+          if origin == destination:
+            return [origin]
+          if heuristic is None: heuristic = lambda x, y: math.sqrt((x[0]-y[0])**2+(x[1]-y[1])**2)
+ 
+          # these are the nodes that have been completely expanded, so don't need to be traced backwards
+          searched = set()
+
+          # these are the nodes still to be explored, sorted by estimated cost. They need to have
+          # the complete path stored because any one of them might contain the best solution. We
+          # arrange this as a nested dictionary to get a reasonably straightforward way to look up
+          # the cheapest path. A heap could also work but introduces implementation complexities.
+
+          expanded = {heuristic(origin, destination): {origin: [origin]}}
+                       
+          while len(expanded) > 0:
+                bestRoute = min(expanded.keys())
+                nextSearch = expanded[bestRoute]
+                if destination in nextSearch:
+                  return nextSearch[destination]
+                nextNode = nextSearch.popitem()
+                while len(nextSearch) > 0 and nextNode[0] in searched:
+                      nextNode = nextSearch.popitem()
+                if len(nextSearch) == 0:
+                   del expanded[bestRoute]
+                if nextNode[0] not in searched:
+                   searched.add(nextNode[0])
+                   nextTargets = [node for node in self._map[nextNode[0]].items() if node[0] not in searched]
+                   while len(nextTargets) > 0:
+                         expTarget = nextTargets.pop()
+                         approxDistance = bestRoute-heuristic(nextNode[0],destination) + heuristic(expTarget[0],destination)
+                         if approxDistance in expanded:             
+                            expanded[approxDistance][expTarget[0]] = nextNode[1] + [expTarget[0]]
+                         else:
+                            expanded[approxDistance] = {expTarget[0]: nextNode[1] + [expTarget[0]]}
+          return []
+
+
       # TODO
       # this function decides whether to offer a bid for a fare. In general you can consider your current position, time,
       # financial state, the collection and dropoff points, the time the fare called - or indeed any other variable that
